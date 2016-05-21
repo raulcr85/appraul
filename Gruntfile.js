377d100b3ab86cae34b129ea49f7acfba5135bc3
jesus-asus <jesusjcatalan@gmail.com>
2016-05-21 17:52:27 +0200

    A file was deleted

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,18 @@
 module.exports = function(grunt) {
 	'use strict';
-	grunt.file.mkdir("D:/xampp/htdocs/appraul");
+	grunt.file.mkdir("c:/wamp64/www/appraul");
 	grunt.initConfig({
 		
 		copy : {
 			main: {
 				files : [
-					{expand: true, src : ['main.html'], dest : 'output'},
+					{expand: true, src : ['index.html'], dest : 'output'},
 					{expand: true, src : ['bower_components/**'] , dest : 'output'}
+				]
+			},
+			index : {
+				files : [
+					{expand: true, src : ['index.html'], dest : 'output'}
 				]
 			}
 		},
@@ -18,17 +23,25 @@ module.exports = function(grunt) {
 		
 		exec : {
 			copyOutput : {
-				command : 'xcopy /y /e output\\* d:\\xampp\\htdocs\\appraul',
+				command : 'xcopy /y /e output\\* c:\\wamp64\\www\\appraul',
+				exitCode : 0
+			},
+			
+			copyIndex : {
+				command : 'xcopy /y /e output\\index.html c:\\wamp64\\www\\appraul',
 				exitCode : 0
 			}
+			
 		}
 	});
 	
+
 	grunt.loadNpmTasks("grunt-contrib-copy");
 	grunt.loadNpmTasks("grunt-contrib-clean");
 	grunt.loadNpmTasks("grunt-exec");
 	
-	grunt.registerTask("default" , ['clean', 'copy', 'exec:copyOutput'] );
+	grunt.registerTask("default" , ['clean', 'copy:main', 'exec:copyOutput'] );
+	grunt.registerTask("index" , [ 'copy:index', 'exec:copyIndex'] );
 	
 };
 
